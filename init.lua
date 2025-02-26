47db2bd6e38aa7d2e08256da5918e81c1d833d31
leddkire <leddkire@gmail.com>
2025-02-26 23:14:17 +0100

    initial vim config

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("customconfig")
+require("config.lazy")
+
+vim.opt.tabstop=8
+vim.opt.softtabstop=8
+vim.opt.shiftwidth=4
+vim.opt.smarttab=true
+vim.opt.expandtab=true
+vim.opt.smartindent=true
+vim.opt.number=true
+
+
---
+++ lua/customconfig/init.lua
@@ -0,0 +1,1 @@
+require("customconfig.remap")
