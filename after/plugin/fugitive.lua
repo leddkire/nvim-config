071b2be19111740eff2040d70215d849f3f2157b
leddkire <leddkire@gmail.com>
2025-03-02 09:28:34 +0100

    Add more fugitive maps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", function() vim.cmd([[:Git push]]) end)
+vim.keymap.set("n", "<leader>gP", function() vim.cmd([[:Git pull]]) end)
+vim.keymap.set("n", "<leader>gPr", function() vim.cmd([[:Git rebase]]) end)
 
