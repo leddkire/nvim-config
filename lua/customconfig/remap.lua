893264ed23e2de5c05c9796532de47ee1e088543
leddkire <leddkire@gmail.com>
2025-03-02 09:56:42 +0100

    Add more remaps

--- lua/customconfig/remap.lua
+++ lua/customconfig/remap.lua
@@ -1,2 +1,32 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--- move selection up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--- Keep cursor at beginning of line when joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+--- Keep cursor in the middle while jumping / searching
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+--- System clipboard yanking
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+--- Todo: keymap for pasting from different registers
+
+vim.keymap.set("n", "Q", "<nop>")
+
+
+-- Quick-fix list. Must learn tool
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
