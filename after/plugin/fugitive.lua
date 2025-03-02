313be9e59f89303b199169f4eb5b01f2030d1724
leddkire <leddkire@gmail.com>
2025-03-02 09:30:35 +0100

    Fix mapping for git rebase

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", function() vim.cmd([[:Git push]]) end)
 vim.keymap.set("n", "<leader>gP", function() vim.cmd([[:Git pull]]) end)
-vim.keymap.set("n", "<leader>gPr", function() vim.cmd([[:Git rebase]]) end)
+vim.keymap.set("n", "<leader>gr", function() vim.cmd([[:Git rebase]]) end)
+
 
