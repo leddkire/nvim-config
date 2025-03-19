7ae738e8ec6bbfe826385fc8846e56590cc9419f
leddkire <leddkire@gmail.com>
2025-03-19 23:45:29 +0100

    Fix undotree for Windows

--- init.lua
+++ init.lua
@@ -5,12 +5,24 @@ require("project_config")
 --- vim.opt.statusline ="%{FugitiveStatusline()}"
 
 vim.o.tabstop=8
+
+
+
 vim.o.softtabstop=8
 vim.o.shiftwidth=4
 vim.o.smarttab=true
 vim.o.expandtab=true
 vim.o.smartindent=true
 vim.o.number=true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
 
 vim.o.termguicolors = true
 vim.o.background = 'light'
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,3 +1,7 @@
+if jit.os == "Windows" then
+    vim.g.undotree_DiffCommand = "FC"
+end
+
 return {
-    "mbbill/undotree"
+    "mbbill/undotree",
 }
