4863f32249b1cf4b60acbc0cc4c8bed971fd5abf
leddkire <leddkire@gmail.com>
2025-03-20 19:16:23 +0100

    Adjust harpoon maps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,12 +7,12 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<M-a>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<M-s>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<M-z>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<M-x>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-`>", function() harpoon:list():next() end)
-vim.keymap.set("n", "<C-1>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<M-z>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<M-x>", function() harpoon:list():prev() end)
 
