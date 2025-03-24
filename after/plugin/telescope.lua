03a3c11ebd994d7751125898e196deffabf24442
leddkire <leddkire@gmail.com>
2025-03-24 10:44:06 +0100

    Fix command description

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 	--- builtin.grep_string({ search = vim.fn.input("Grep > ") });
 --- end)
 vim.keymap.set('n', '<leader>fw', builtin.grep_string)
-vim.keymap.set('n', '<leader>fr', builtin.git_files, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fr', builtin.git_files, { desc = 'Telescope git files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
