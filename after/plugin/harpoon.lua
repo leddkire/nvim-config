4e09536066e251f8681794082454af81ae3e3ee2
leddkire <leddkire@gmail.com>
2025-03-31 09:13:08 +0200

    Invert harpoon navigation bindings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -13,6 +13,6 @@ vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<M-o>", function() harpoon:list():next() end)
-vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<M-p>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<M-o>", function() harpoon:list():prev() end)
 
