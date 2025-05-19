1be9113995051743e66baaac52f73f8048ed1b3a
leddkire <leddkire@gmail.com>
2025-05-19 09:41:23 +0200

    Add Format command

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,11 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
+    'stevearc/conform.nvim',
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
+        },
+    }
 }
