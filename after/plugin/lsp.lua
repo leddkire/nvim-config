e6b20c3688cc66a4c91e180b03f2b27cf2523128
leddkire <leddkire@gmail.com>
2025-06-05 15:27:32 +0200

    Add tab to jump when inside a snippet

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -113,3 +113,11 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
 })
+
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+   if vim.snippet.active({ direction = 1 }) then
+     return '<cmd>lua vim.snippet.jump(1)<cr>'
+   else
+     return '<Tab>'
+   end
+ end, { expr = true })
