f93a6b8f33f74bdab8d0988eded65bbae6d0bd52
leddkire <leddkire@gmail.com>
2025-06-05 10:28:44 +0200

    Add treesitter module for nginx

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"vimdoc", "javascript", "typescript", "lua", "gdscript", "c" },
+    ensure_installed = {"vimdoc", "javascript", "typescript", "lua", "gdscript", "c", "nginx" },
     sync_install = false,
     auto_install = true,
     highlight = {
