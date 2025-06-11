0a9c72e0e25f4b1c0cc567c67d289ef0daf928f3
leddkire <leddkire@gmail.com>
2025-06-11 09:22:13 +0200

    Remove display of whitespace characters

--- init.lua
+++ init.lua
@@ -57,8 +57,8 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = true
-vim.opt.listchars = { tab = '->', trail = '+', nbsp = '?' }
+-- vim.o.list = true
+-- vim.opt.listchars = { tab = '->', trail = '+', nbsp = '?' }
 
 vim.o.inccommand = 'split'
 
