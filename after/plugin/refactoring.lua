8d8ea7921405528c8a097cb0658ea960cf4c430e
leddkire <leddkire@gmail.com>
2025-09-06 08:39:24 +0200

    Added description for refactoring mappings

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,16 +1,16 @@
 require("refactoring").setup({ })
 
-vim.keymap.set("x", "<leader>re", ":Refactor extract ")
-vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+vim.keymap.set("x", "<leader>re", ":Refactor extract ", {desc="Refactor: extract"})
+vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ", {desc = "Refactor: extract to file"})
 
-vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ", { desc = "Refactor: extract variable" })
 
-vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var", { desc = "Refactor: inline variable" })
 
-vim.keymap.set( "n", "<leader>rI", ":Refactor inline_func")
+vim.keymap.set( "n", "<leader>rI", ":Refactor inline_func", { desc = "Refactor: inline func" })
 
-vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
-vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+vim.keymap.set("n", "<leader>rb", ":Refactor extract_block", { desc = "Refactor: extract block" })
+vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file", { desc = "Refactor extract block to file" })
 
 
 -- You can also use below = true here to to change the position of the printf
@@ -19,12 +19,12 @@ vim.keymap.set(
 	"n",
 	"<leader>rp",
 	function() require('refactoring').debug.printf({below = false}) end
-)
+, { desc = "Add debug print statement" })
 
 -- Print var
 
-vim.keymap.set({"x", "n"}, "<leader>rpv", function() require('refactoring').debug.print_var() end)
+vim.keymap.set({"x", "n"}, "<leader>rpv", function() require('refactoring').debug.print_var() end, { desc = "Add debug print statement with variables" })
 -- Supports both visual and normal mode
 
-vim.keymap.set("n", "<leader>rpc", function() require('refactoring').debug.cleanup({}) end)
+vim.keymap.set("n", "<leader>rpc", function() require('refactoring').debug.cleanup({}) end, { desc = "Clean-up debug print statements" })
 -- Supports only normal mode
