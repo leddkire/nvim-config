1cd9d6d166b893aafe319c5212fd798558360987
leddkire <leddkire@gmail.com>
2025-10-13 11:17:55 +0200

    Update todo in copilot plugin

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -6,9 +6,10 @@ vim.g.copilot_no_tab_map = true
 vim.cmd.Copilot("disable")
 
 local copilot_enabled = false
--- Todo: Create statusline module with API for adding items to the status line
+-- TODO: Create statusline module with API for adding items to the status line
 -- Then require the module here and add a 'GH' at the end of the statusline when
 -- copilot is enabled.
+-- TODO: Integrate with telescope
 vim.keymap.set({'n', 'i'}, '<F9>', function ()
     if copilot_enabled then
         vim.cmd.Copilot("disable")
