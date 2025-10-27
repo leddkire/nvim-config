96d5b04ca87df10f1c8d3adaf37da9404dbd57a1
leddkire <leddkire@gmail.com>
2025-10-27 16:18:35 +0100

    fix incorrect harpoon mappings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,8 +7,8 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<M-p>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<M-o>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-o>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-p>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<M-[>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<M-]>", function() harpoon:list():select(4) end)
 
