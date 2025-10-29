27a728336e7af321d620e028f2c3fbc3aa8b3c96
leddkire <leddkire@gmail.com>
2025-10-29 12:52:29 +0100

    Add lua function for dumping vim cmd output to new buffer

--- lua/customconfig/globals.lua
+++ lua/customconfig/globals.lua
@@ -2,3 +2,14 @@ P = function (v)
     print(vim.inspect(v))
     return v
 end
+
+DumpCmd = function (cmd, split_direction)
+    if(split_direction == 'v') then
+        vim.cmd.vs()
+    else
+        vim.cmd.sp()
+    end
+    vim.cmd.enew()
+    vim.cmd("put=execute('" .. cmd .. "')")
+end
+
