882b1a74627582f104fe126a921888d678831356
leddkire <leddkire@gmail.com>
2026-02-15 08:47:39 +0100

    Make put text functions local

--- lua/sandbox/put_text.lua
+++ lua/sandbox/put_text.lua
@@ -1,6 +1,6 @@
 print("hello world")
 -- TODO: decouple mappings from descriptions and functions so they can be reused by different sandboxes
-function test_nvim_paste()
+local function test_nvim_paste()
     local phase = -1 -- Don't stream
     local break_at_cr_and_clrf = false
     vim.api.nvim_paste([[
@@ -10,7 +10,7 @@ function test_nvim_paste()
 end
 vim.keymap.set('n','<leader>tu', function () test_nvim_paste() end,  { desc = "test nvim_paste" })
 
-function test_nvim_put()
+local function test_nvim_put()
     local after_cursor = false
     local place_cursor_at_end = true
     vim.api.nvim_put({
@@ -20,13 +20,23 @@ function test_nvim_put()
 end
 vim.keymap.set('n','<leader>tj', function () test_nvim_put() end, { desc = "test nvim_put"})
 
-function test_nvim_buf_set_lines()
+local current_buffer = 0
+local function test_nvim_buf_set_lines()
     print('test_nvim_buf_set_lines')
+    local start_line = -1
+    local end_line = -1
+    local buffer = current_buffer
+    vim.api.nvim_buf_set_lines(buffer, start_line, end_line, false, {"line1", "line2"})
 end
 vim.keymap.set('n','<leader>tk', function () test_nvim_buf_set_lines() end, { desc = "test nvim_buf_set_lines"})
 
-function test_nvim_buf_set_text()
+local function test_nvim_buf_set_text()
     print('test_nvim_buf_set_text')
+    local buffer = current_buffer
+    local start_row = -1
+    local start_col = 74
+    local end_row = -1
+    local end_col = 74
+    vim.api.nvim_buf_set_text(buffer, start_row, start_col, end_row, end_col, {"replacement"} )
 end
 vim.keymap.set('n','<leader>ti', function () test_nvim_buf_set_text() end, { desc = "test nvim_buf_set_text"})
-
