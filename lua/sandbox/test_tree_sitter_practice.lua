89f1b791c1740f7dec906ea248c94a0a7d87e61d
leddkire <leddkire@gmail.com>
2026-02-21 11:41:00 +0100

    Enable testing modules with MiniTest

--- lua/sandbox/test_tree_sitter_practice.lua
+++ lua/sandbox/test_tree_sitter_practice.lua
@@ -1,29 +1,58 @@
 local new_set = MiniTest.new_set
 local expect = MiniTest.expect
 local ts = vim.treesitter
-local tree_sitter_practice = require('sandbox.tree_sitter_practice')
+-- Module must be loaded this way in order to test any changes made to it.
+-- If instead we use require, then I would need to add some fancy module reloading on
+--   a child neovim instance
+dofile('lua/sandbox/tree_sitter_practice.lua')
 
 T = new_set()
 T['base cases'] = new_set()
-T['base cases']['returns the local variable declaration before the cursor'] = function ()
-    expect.equality(1, 2)
-          --   local content = [[
-          --   local foo = 2
-          --   print("hello")
-          -- ]]
-          -- local language_trees = ts.get_string_parser(content, "lua")
-          -- local tree = language_trees[1]
-          -- P(tree)
-          -- local query = [[
-          --   (function_call (identifier) @name (#eq? @name "print"))
-          -- ]]
-          -- P(query)
-          -- local parsed_query = ts.query.parse("lua", query)
-          -- for _id, node, _metadata, _match in parsed_query:iter_matches(tree:root(), content) do
-          --     T(node)
-          --     local local_declarations = tree_sitter_practice.get_local_declarations(node, tree)
-          --     assert.array_equals(local_declarations, {"foo"})
-          -- end
+T['base cases']['returns and empty array when content is empty'] = function ()
+    local content = ""
+    local result = M.get_local_declarations(content)
+    expect.equality(result, {})
+end
+T['base cases']['returns the local declaration if it is the only line'] = function ()
+    local content = "local foo = 1"
+    local result = M.get_local_declarations(content)
+    expect.equality(result, {"foo"})
+end
+T['base cases']['returns both declarations if they are in the same line'] = function ()
+    local content = "local foo, bar = 1, 2"
+    local result = M.get_local_declarations(content)
+    expect.equality(result, {"foo", "bar"})
+end
+T['base cases']['returns both declarations if they are in different lines'] = function ()
+    local content = [[
+        local foo = 1
+        local bar = 2
+    ]]
+    local result = M.get_local_declarations(content)
+    expect.equality(result, {"foo", "bar"})
+end
+T['base cases']['returns the local variable declaration before the cursor'] = function()
+    -- local content = [[
+    --         local foo = 2
+    --         print("hello")
+    --       ]]
+    -- local parser = ts.get_string_parser(content, "lua")
+    -- local tree = parser:parse()[1]
+    -- local root = tree:root()
+    -- local query = [[
+    --         (function_call (identifier) @name (#eq? @name "print"))
+    --       ]]
+    -- P(query)
+    -- local parsed_query = ts.query.parse("lua", query)
+    -- local results = {}
+    -- for _id, node, _metadata, _match in parsed_query:iter_matches(root, content) do
+    --     T(node)
+    --     local local_declarations = tree_sitter_practice.get_local_declarations(node, tree)
+    --     for elem in local_declarations do
+    --         table.insert(results, elem)
+    --     end
+    -- end
+    -- expect.equality(results, { "foo" })
 end
 
 return T
--- lua/sandbox/tree_sitter_practice.lua
+++ lua/sandbox/tree_sitter_practice.lua
@@ -115,5 +115,28 @@ vim.keymap.set('n', '<leader>tw', function ()
 end)
 
 -- I can create treesitter queries and execute them to get the information I need as well. I should investigate how to do that too.
+
 M = {}
+M.get_local_declarations = function (content)
+    if content == "" then
+        return {}
+    end
+
+    local parser = ts.get_string_parser(content, "lua")
+    local tree = parser:parse()[1]
+    local root = tree:root()
+    local query_string = [[
+        ((identifier) @id)
+    ]]
+    local query = ts.query.parse("lua", query_string)
+
+    local declaration_list = {}
+    ---@diagnostic disable-next-line: unused-local
+    for id, node, metadata in query:iter_captures(root, 0) do
+        local node_text = ts.get_node_text(node, content)
+        table.insert(declaration_list, node_text)
+    end
+
+    return declaration_list
+end
 return M
