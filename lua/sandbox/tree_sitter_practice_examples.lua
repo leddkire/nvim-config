684eba6cf7eee8a19bb674d0d8eeeba6df711c8c
leddkire <leddkire@gmail.com>
2026-03-07 07:35:04 +0100

    Add lua example for practicing tree sitter

---
+++ lua/sandbox/tree_sitter_practice_examples.lua
@@ -0,0 +1,17 @@
+local outer = 1
+
+function pii(input_param)
+    local zep
+end
+
+if 1 == 2 then
+    local bar
+    if 1 == 4 then
+        local derp
+    else
+        local morp
+    end
+end
+if 1 == 2 then
+    local foo
+end
