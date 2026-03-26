ddc8241136e91a3976e7ff919e02920e1262c1ff
leddkire <leddkire@gmail.com>
2026-03-26 22:00:42 +0100

    Add a bunch of cool plugins

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,35 @@
+-- lazy.nvim
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        cmdline = {
+            view = "cmdline"
+        },
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        routes = {
+            {
+                filter = {
+                    event = "msg_show",
+                    any = {
+                        { find = "%d+L, %d+B" },
+                        { find = "; after #%d+" },
+                        { find = "; before #%d+" },
+                    },
+                },
+                view = "mini",
+            },
+        },
+        presets = {
+            bottom_search = true,
+            command_palette = false,
+            long_message_to_split = true,
+        },
+    }
+}
